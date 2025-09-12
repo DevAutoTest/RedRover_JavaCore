0af67cdb5737f46c41a1ca488ad3d085e6fc5fc6
tumba7788 <Elena_Danilova7788@outlook.com>
2025-09-12 16:20:01 +0200

    Lessons7-8

---
+++ src/Lesson7_Methods/TypeCasting.java
@@ -0,0 +1,31 @@
+package Lesson7_Methods;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        /*
+        Надо найти ошибки при сужении типа
+        */
+
+//        int i = 3;
+//        byte b = 1;
+//        byte b1 = 1 + 2;                // line 1 константное выражение вписывается в byte (будет 3)
+//        short s = 304111;               // line 2  Максимум: 32767
+//        short s1 = (short) 304111;      // line 3
+//        b = b1 + 1;                     // line 4 binary numeric promotion b1 = int и 1 это литерал int (искл если b1 будет final - компилятор ее сразу посчитает)
+//        b = (byte) (b1 + 1);           // line 5
+//        b = -b;                         // line 6
+//        b = (byte) -b;                  // line 7
+//        b1 *= 2;                        // line 8
+//        b = i;                          // line 9
+//        b = (byte) i;
+//        b += i++;
+//        float f = 1.1f;
+//        b /= f;
+//
+//        float f2 = 1 / 2;
+//        int i2 = 1 / 3;
+//        float f3 = 1.45;
+//        double d = 555d;
+
+    }
+}
